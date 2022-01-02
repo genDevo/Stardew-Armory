97800059742e7c8e20481cf50cc5be22a8b2d4a4
genDevo <62770601+genDevo@users.noreply.github.com>
2022-01-01 21:57:44 -0600

    Init Fabric Commit

--- src/main/java/net/gendevo/stardewarmory/StardewArmory.java
+++ src/main/java/net/gendevo/stardewarmory/StardewArmory.java
@@ -1,198 +1,21 @@
 package net.gendevo.stardewarmory;
 
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMultimap;
-import com.mojang.serialization.Codec;
-import net.gendevo.stardewarmory.config.StardewArmoryConfig;
-import net.gendevo.stardewarmory.items.ModItemTier;
-import net.gendevo.stardewarmory.network.ModNetwork;
-import net.gendevo.stardewarmory.screen.GalaxyForgeScreen;
-import net.gendevo.stardewarmory.setup.*;
-import net.gendevo.stardewarmory.util.IridiumModeGui;
-import net.gendevo.stardewarmory.util.KeybindSetup;
-import net.gendevo.stardewarmory.util.ModResourceLocation;
-import net.gendevo.stardewarmory.world.OreGeneration;
-import net.gendevo.stardewarmory.world.structures.ConfiguredStructureInit;
-import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.core.Registry;
-import net.minecraft.data.BuiltinRegistries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.chunk.ChunkGenerator;
-import net.minecraft.world.level.levelgen.FlatLevelSource;
-import net.minecraft.world.level.levelgen.StructureSettings;
-import net.minecraft.world.level.levelgen.feature.ConfiguredStructureFeature;
-import net.minecraft.world.level.levelgen.feature.StructureFeature;
-import net.minecraft.world.level.levelgen.feature.configurations.StructureFeatureConfiguration;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.world.WorldEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
+import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import top.theillusivec4.curios.api.SlotTypeMessage;
-import top.theillusivec4.curios.api.SlotTypePreset;
 
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
+public class StardewArmory implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LogManager.getLogger("stardewarmory");
 
-@Mod(StardewArmory.MOD_ID)
-public class StardewArmory
-{
-    public static final String MOD_ID = "stardewarmory";
-    public static final Logger LOGGER = LogManager.getLogger();
-    public static final CreativeModeTab TAB_STARDEW = new StardewGroup("stardewtab");
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
 
-    public StardewArmory() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, StardewArmoryConfig.SPEC, "stardewarmory-common.toml");
-        Registration.register(modEventBus);
-
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
-        //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::loadComplete);
-
-        // Register ourselves for server and other game events we are interested in
-        IEventBus forgeBus = MinecraftForge.EVENT_BUS;
-        forgeBus.register(this);
-
-        //forgeBus.register(new IridiumModeGui(Minecraft.getInstance()));
-        ModStructures.DEFERRED_REGISTRY_STRUCTURE.register(modEventBus);
-
-        if (StardewArmoryConfig.guild_spawn.get()) {
-            forgeBus.addListener(EventPriority.NORMAL, this::addDimensionalSpacing);
-        }
-    }
-
-    private void setup(final FMLCommonSetupEvent event)
-    {
-        ModNetwork.init();
-        ModItemTier.init();
-        event.enqueueWork(() -> {
-            if (StardewArmoryConfig.guild_spawn.get()) {
-                ModStructures.setupStructures();
-                ConfiguredStructureInit.registerConfiguredStructures();
-            }
-            OreGeneration.registerOres();
-        });
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        KeybindSetup.register(event);
-        IridiumModeGui.register();
-        event.enqueueWork(() -> MenuScreens.register(ModContainers.GALAXY_FORGE_CONTAINER.get(), GalaxyForgeScreen::new));
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.RING.getMessageBuilder().size(2).build());
-    }
-
-    private static Method GETCODEC_METHOD;
-    public void addDimensionalSpacing(final WorldEvent.Load event) {
-        if(event.getWorld() instanceof ServerLevel serverLevel){
-            ChunkGenerator chunkGenerator = serverLevel.getChunkSource().getGenerator();
-            if (chunkGenerator instanceof FlatLevelSource && serverLevel.dimension().equals(Level.OVERWORLD)) {
-                return;
-            }
-
-            StructureSettings worldStructureConfig = chunkGenerator.getSettings();
-
-            HashMap<StructureFeature<?>, HashMultimap<ConfiguredStructureFeature<?, ?>, ResourceKey<Biome>>> STStructureToMultiMap = new HashMap<>();
-
-            for(Map.Entry<ResourceKey<Biome>, Biome> biomeEntry : serverLevel.registryAccess().ownedRegistryOrThrow(Registry.BIOME_REGISTRY).entrySet()) {
-                Biome.BiomeCategory biomeCategory = biomeEntry.getValue().getBiomeCategory();
-                if(biomeCategory != Biome.BiomeCategory.OCEAN && biomeCategory != Biome.BiomeCategory.THEEND && biomeCategory != Biome.BiomeCategory.NETHER && biomeCategory != Biome.BiomeCategory.NONE) {
-                    associateBiomeToConfiguredStructure(STStructureToMultiMap, ConfiguredStructureInit.CONFIGURED_GUILD_BUILDING, biomeEntry.getKey());
-                }
-            }
-
-            ImmutableMap.Builder<StructureFeature<?>, ImmutableMultimap<ConfiguredStructureFeature<?, ?>, ResourceKey<Biome>>> tempStructureToMultiMap = ImmutableMap.builder();
-            worldStructureConfig.configuredStructures.entrySet().stream().filter(entry -> !STStructureToMultiMap.containsKey(entry.getKey())).forEach(tempStructureToMultiMap::put);
-
-            STStructureToMultiMap.forEach((key, value) -> tempStructureToMultiMap.put(key, ImmutableMultimap.copyOf(value)));
-
-            worldStructureConfig.configuredStructures = tempStructureToMultiMap.build();
-
-            try {
-                if(GETCODEC_METHOD == null) GETCODEC_METHOD = ObfuscationReflectionHelper.findMethod(ChunkGenerator.class, "codec");
-                ResourceLocation cgRL = Registry.CHUNK_GENERATOR.getKey((Codec<? extends ChunkGenerator>) GETCODEC_METHOD.invoke(chunkGenerator));
-                if(cgRL != null && cgRL.getNamespace().equals("terraforged")) return;
-            }
-            catch(Exception e){
-                StardewArmory.LOGGER.error("Was unable to check if " + serverLevel.dimension().location() + " is using Terraforged's ChunkGenerator.");
-            }
-
-            if(chunkGenerator instanceof FlatLevelSource &&
-                    serverLevel.dimension().equals(Level.OVERWORLD)){
-                return;
-            }
-
-            Map<StructureFeature<?>, StructureFeatureConfiguration> tempMap = new HashMap<>(worldStructureConfig.structureConfig());
-            tempMap.putIfAbsent(ModStructures.GUILD_BUILDING.get(), StructureSettings.DEFAULTS.get(ModStructures.GUILD_BUILDING.get()));
-            worldStructureConfig.structureConfig = tempMap;
-        }
-    }
-
-    private static void associateBiomeToConfiguredStructure(Map<StructureFeature<?>, HashMultimap<ConfiguredStructureFeature<?, ?>, ResourceKey<Biome>>> STStructureToMultiMap, ConfiguredStructureFeature<?, ?> configuredStructureFeature, ResourceKey<Biome> biomeRegistryKey) {
-        STStructureToMultiMap.putIfAbsent(configuredStructureFeature.feature, HashMultimap.create());
-        HashMultimap<ConfiguredStructureFeature<?, ?>, ResourceKey<Biome>> configuredStructureToBiomeMultiMap = STStructureToMultiMap.get(configuredStructureFeature.feature);
-        if(configuredStructureToBiomeMultiMap.containsValue(biomeRegistryKey)) {
-            StardewArmory.LOGGER.error("""
-                    Detected 2 ConfiguredStructureFeatures that share the same base StructureFeature trying to be added to same biome. One will be prevented from spawning.
-                    This issue happens with vanilla too and is why a Snowy Village and Plains Village cannot spawn in the same biome because they both use the Village base structure.
-                    The two conflicting ConfiguredStructures are: {}, {}
-                    The biome that is attempting to be shared: {}
-                """,
-                    BuiltinRegistries.CONFIGURED_STRUCTURE_FEATURE.getId(configuredStructureFeature),
-                    BuiltinRegistries.CONFIGURED_STRUCTURE_FEATURE.getId(configuredStructureToBiomeMultiMap.entries().stream().filter(e -> e.getValue() == biomeRegistryKey).findFirst().get().getKey()),
-                    biomeRegistryKey
-            );
-        }
-        else{
-            configuredStructureToBiomeMultiMap.put(configuredStructureFeature, biomeRegistryKey);
-        }
-    }
-
-    public static ModResourceLocation getId(String path) {
-        if (path.contains(":")) {
-            throw new IllegalArgumentException("path contains namespace");
-        }
-        return new ModResourceLocation(path);
-    }
-
-    //Adds creative tab
-    @Mod.EventBusSubscriber
-    public static class StardewGroup extends CreativeModeTab {
-        public StardewGroup(String label) {
-            super("stardewtab");
-        }
-
-        @Override
-        public ItemStack makeIcon() {
-            return ModItems.PRISMATIC_SHARD.get().getDefaultInstance();
-        }
-    }
-
+		LOGGER.info("Hello Fabric world!");
+	}
 }
